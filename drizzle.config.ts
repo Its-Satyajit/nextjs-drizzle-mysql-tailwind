8071606faa076e2e852031e9a119b81a64019ff9
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2024-07-29 23:23:22 +0530

    🐳 chore: Update Dependencies.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     schema: './src/db/schema/*',
     out: 'drizzle',
     dbCredentials: {
-        url: process.env.DB_URL,
+        host: process.env.DB_HOST as string,
+        user: process.env.DB_USER as string,
+        password: process.env.DB_PASSWORD as string,
+        database: process.env.DB_NAME as string,
+        port: Number(process.env.DB_PORT),
     },
 });
